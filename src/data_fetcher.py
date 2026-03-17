f6d4521366900a44db1b6c1f7d1026b93a25681e
crisdimxs <cristiandimas1945@gmail.com>
2026-03-16 20:20:20 -0500

    Fourth commit: data_fetcher.py has been changed a lot, for the future main.py will can connect to this file better

--- src/data_fetcher.py
+++ src/data_fetcher.py
@@ -5,25 +5,33 @@ import time
 from rich.console import Console
 from src.config import TICKER, TIME_FRAME, DATA_PATH
 
-def fetch_historical_data(limit=10000):
+process = ["[ x ]", "[ + ]", "[ ! ]", "[ * ]"]
+
+def fetch_historical_data(limit=10000, show_status=True):
     exchange = ccxt.binance()
     
-    with Console().status(f"[bold white] Connecting to Binance for download {limit} candles for {TICKER}..."):
+    def download():
         ohlcv = exchange.fetch_ohlcv(TICKER, timeframe=TIME_FRAME, limit=limit)
-        
-    Console().print(f"[bold green]\[ + ] Succesfully connected to Binance")
     
-    with Console().status("[bold white] Processing data and saving to CSV..."):
         df = pd.DataFrame(ohlcv, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
         df['date'] = pd.to_datetime(df['timestamp'], unit='ms')
-        df = df.dropna() 
+        df = df.dropna()
         df['tic'] = TICKER
         df = df[["date", "open", "high", "low", "close", "volume", "tic"]]
-        
-        df.to_csv(DATA_PATH, index=False)
 
-    Console().print(f"[bold green]\[ + ] Data saved in  {DATA_PATH}")
-    return df
+        df.to_csv(DATA_PATH, index=False)    
+        
+        return df
+
+    if show_status:
+        with Console().status(f"[bold white] Connecting to Binance for download {limit} candles for {TICKER}..."):
+            df = download()
+        
+        Console().print(f"[bold green]{process[1]} " + "[bold white]Succesfully connected to Binance")
+        Console().print(f"[bold green]{process[1]}" + f"[bold white] Data saved in  {DATA_PATH}")
+
+    else:
+        return download()
 
 if __name__ == "__main__":
     fetch_historical_data()
